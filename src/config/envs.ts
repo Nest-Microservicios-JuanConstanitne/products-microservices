841cdd7bc4b82f5cc16daa070d8e3689d9510b2d
juanovni <constantinemurillo@gmail.com>
2026-02-20 12:03:16 -0500

    feat: add nats transport and update orders service

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,13 +3,21 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+
+  NATS_SERVERS: string[];
 }
 
 const envSchema = joi.object({
-  PORT: joi.number().required()
+  PORT: joi.number().required(),
+
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
+
 }).unknown(true);
 
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 if (error) {
   throw new Error(`Error undefined env ${error.message}`)
@@ -19,4 +27,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+
+  natsServers: envVars.NATS_SERVERS,
 }
